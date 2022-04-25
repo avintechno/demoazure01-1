05414c40f9f1c8c79aac299d708dce52c2546f94
avintechno <avintechno@gmail.com>
2022-04-25 07:12:19 +0000

    Updated HomeController.cs : syntax Error

--- DemoWebApp/DemoWebApp/Controllers/HomeController.cs
+++ DemoWebApp/DemoWebApp/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace DemoWebApp.Controllers
     public class HomeController : Controller
     {
         public ActionResult Index()
-        {
+        
             return View();
         }
 
